c8262ccb3491bf28a2092a256c2173d385e91f5e
souad5017 <souadelbarjiji@gmail.com>
2026-09-01 15:39:38 +0100

    first commit

---
+++ exercice-01.js
@@ -0,0 +1,19 @@
+const prenom = "Amina";
+const ville = "Casablanca";
+let age = 22;
+let estEnFormation = true;
+
+
+
+console.log(`${prenom} , ${age} , ${ville}`);
+
+if (age >= 18 ){
+    console.log('status : majeur');
+}else {
+    console.log('mineur');
+}
+
+console.log('prenom :', typeof prenom);
+console.log('age :', typeof age);
+console.log('ville :', typeof ville);
+console.log('estEnFormation :', typeof estEnFormation);
